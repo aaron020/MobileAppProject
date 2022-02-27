c2a9c991df52c0bb4bb4e375ba32f76bf0efa798
Aaron Meade <64416980+aaron020@users.noreply.github.com>
2022-02-27 12:23:42 +0000

    FireBase + login update

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -1,66 +1,153 @@
 package com.example.mobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextLoginEmailAddress;
-    EditText editTextPassword;
+    EditText editTextLoginPassword;
     Button buttonLogin;
     ProgressBar progressBarLogin;
+    FirebaseAuth fAuth;
     private String email;
     private String password;
+    private boolean emailFilled = false;
+    private boolean passwordFilled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Creates all the global variables
         createElements();
+        //Listener to check if the login button has been pressed
+        clickLoginListener();
+        //Listener to check if the email and password have been entered
+        //Only if they have both been entered will the Login button be enabled
+        verifyFieldsListener();
+
+    }
+
+    private void login(String email, String password){
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(MainActivity.this,"Missing Field!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Signed In", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    progressBarLogin.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
 
 
 
 
+    private void verifyFieldsListener(){
+        editTextLoginEmailAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!(editTextLoginEmailAddress.getText().toString().isEmpty())){
+                    emailFilled = true;
+                }else{
+                    emailFilled = false;
+                    buttonLogin.setEnabled(false);
+                }
+
+                if(emailFilled && passwordFilled){
+                    buttonLogin.setEnabled(true);
+                }
+
+            }
+        });
+
+        editTextLoginPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!(editTextLoginPassword.getText().toString().isEmpty()) && verifyPassword(editTextLoginPassword.getText().toString())){
+                    passwordFilled = true;
+                }else{
+                    passwordFilled = false;
+                    buttonLogin.setEnabled(false);
+                }
+
+                if(emailFilled && passwordFilled){
+                    buttonLogin.setEnabled(true);
+                }
+
+                System.out.println("Password listener " + emailFilled + "  " + passwordFilled);
+
+            }
+        });
+
+    }
+
+
+    private boolean verifyPassword(String password){
+        if(password.length() < 8){
+            return false;
+        }else{
+            return true;
+        }
+    }
 
 
 
+    private void clickLoginListener(){
         //Click the login button
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Progress Bar is turned on
                 progressBarLogin.setVisibility(View.VISIBLE);
-
-
-
                 email = editTextLoginEmailAddress.getText().toString().trim();
-                password = editTextPassword.getText().toString().trim();
-
-                System.out.println(email + "   " + password);
-
+                password = editTextLoginPassword.getText().toString().trim();
+                login(email,password);
             }
         });
-
-
-
-
-
     }
-
-
-
 
 
     private void createElements(){
         editTextLoginEmailAddress = findViewById(R.id.editTextLoginEmailAddress);
-        editTextPassword = findViewById(R.id.editTextLoginPassword);
+        editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setEnabled(false);
         progressBarLogin = findViewById(R.id.progressBarLogin);
+        fAuth = FirebaseAuth.getInstance();
     }
 
 
